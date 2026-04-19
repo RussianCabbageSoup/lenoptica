9291e27808177c323f50661b1417d871768fc28a
RussianCabbageSoup <testylya333@gmail.com>
2026-04-19 11:24:03 +0300

    update file structure

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,13 +1,28 @@
-const { User } = require("../models/models");
+const { User, Basket } = require("../models/models");
 const ApiError = require('../error/ApiError');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 class UserController {
     async registration(req, res) {
-        
+        const {name, email, password, role} = req.body
+        if (!email || !password) {
+            return next(ApiError.badRequest('Некорректный адрес электронной почты или пароль'))
+        }
+        const candidate = await User.findOne({
+            where: {email}
+        })
+        if (candidate) {
+            return next(ApiError.badRequest('Пользователь уже существует'))
+        }
+        const hashPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({name, email, password: hashPassword, role})
+        const basket = await Basket.create({userId: user.id})
+        const jwt = jwt.sign({id: user.id, email: email, role}, )
     }
 
     async login(req, res) {
-                                                                    //  56:30
+
     }
 
     async chechAuth(req, res, next) {
